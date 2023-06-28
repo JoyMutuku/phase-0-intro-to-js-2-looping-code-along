ae4ae50b9129087694a6e176d7aaf8cf1042a4cc
Joy <joym.mwende@gmail.com>
2023-06-28 12:27:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names,event){
+    let messages=[]
+    for(let n=0; n<names.length; n++){
+        let gratitude=`Thank you, ${names[n]}, for the wonderful ${event} gift!`
+    messages.push(gratitude)
+    }
+    return messages
+
+}
+function countDown(number){
+    for(let n=number; n>=0; n--){
+        console.log(n);
+    }
+}
